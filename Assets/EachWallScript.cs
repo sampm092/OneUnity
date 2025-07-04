17cda2c4bd25e84e0c8598f0c6b09ac2e7de6e13
SamPM092 <sammich4294@gmail.com>
2025-07-04 14:28:01 +0700

    simplify each wall script

--- Assets/EachWallScript.cs
+++ Assets/EachWallScript.cs
@@ -1,15 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Walltype { destroyable, undestroyable};
 public class EachWallScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public AudioClip destroyableSound;
+    public AudioClip undestroyableSound;
+    public AudioClip passSound;
+    private AudioSource audioSource;
+    public Walltype walltype;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Bullet"))
+        if (!collision.gameObject.CompareTag("Bullet")) return;
+
+        AudioClip clipToPlay = null;
+
+        switch (walltype)
         {
-            Destroy(gameObject);
+            case Walltype.destroyable:
+                Destroy(gameObject);
+                clipToPlay = destroyableSound;
+                break;
+            case Walltype.undestroyable:
+                clipToPlay = undestroyableSound;
+                break;
         }
+
+        if (clipToPlay != null)
+            audioSource.PlayOneShot(clipToPlay);
     }
 }
